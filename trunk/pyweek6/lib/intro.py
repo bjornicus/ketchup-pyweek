3e068a87a2bf0323d31094ee98e19aeb4662e40b
colalord <colalord@46f8a242-7348-0410-acc9-3907ed1566ce>
2008-04-01 06:14:15 +0000

    Adjusted intro to switch to the main menu after an interval passes. The interval is set to 5 seconds.

--- trunk/pyweek6/lib/intro.py
+++ trunk/pyweek6/lib/intro.py
@@ -2,12 +2,22 @@ from pyglet import event
 from pyglet import image
 import data
 
+# Timer duration in seconds.
+timerDuration = 5
+
 class Intro(event.EventDispatcher):
     def __init__(self):
-        pass
+        # Reset timer.
+        self.timerCurrent = 0
 
     def update(self,dt):
         self.image.blit(0,0)
+        # Check for timer duration.
+        if self.timerCurrent > timerDuration:
+            self.finish()
+        else:
+            # Increment timer.
+            self.timerCurrent += dt
 
     def on_key_press(self, symbol, modifiers):
         self.finish()
