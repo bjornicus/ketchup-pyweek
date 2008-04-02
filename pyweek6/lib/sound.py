970ff3278be877a2706463a3a0ad7718fe6a2a88
colalord <colalord@46f8a242-7348-0410-acc9-3907ed1566ce>
2008-04-02 17:15:00 +0000

    changed SoundManager.bufferMusic() to soundManager.buffer() to buffer both music and sfx.
    re-scoped the soundManager to be in the name space of the main module.
    
    adjusted the soundmanager.playMusic(filename)
    - playMusic(filename = defaultMusic) now has a default value of filename = defaultMusic.
    - playMusic() now stops any currently playing music before starting new track.

--- pyweek6/lib/sound.py
+++ pyweek6/lib/sound.py
@@ -2,31 +2,34 @@ from pyglet import media
 import data
 
 class SoundManager:
-    # Data Attributes
     defaultMusic = data.filepath("robot maker.mp3")
-    lastMusic = defaultMusic
-    playingMusic = False
     
     def __init__(self):
+        self.lastMusic = self.defaultMusic
+        self.playingMusic = False
+        
         self.smMusic = media.Player()
         self.smSFX = media.Player()
     
     # Streaming BG Music
-    def playMusic(self, filename):
+    def playMusic(self, filename = defaultMusic):
+        self.stopMusic()
         self.playingMusic = True
         self.lastMusic = filename
         musicCD = media.load(filename)
         self.smMusic.queue(musicCD)
         self.smMusic.play()
     
-    def bufferMusic(self):
+    def stopMusic(self):
+        self.playingMusic = False
+        self.smMusic.pause()
+        self.smMusic.next()
+    
+    # Buffering for BG Music and SFX
+    def buffer(self):
         if self.playingMusic:
             if self.smMusic.source:
                 self.smMusic.dispatch_events()
             else:
                 self.playMusic(self.lastMusic)
     
-    def stopMusic(self):
-        self.playingMusic = False
-        self.smMusic.pause()
-        self.smMusic.next()
