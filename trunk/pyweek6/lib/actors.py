e4f43e5e763eca27b1f98f25352469e558dce558
holomorph <holomorph@46f8a242-7348-0410-acc9-3907ed1566ce>
2008-04-02 02:20:33 +0000

    fixed newlines in credits (needs test on windows) added basic actors.py skeleton (to be filled in) moved conveyor to actors.py and defined other actor subclasses modified intro to have the background image defined in the class definition, also transition to menu on mous click

---
+++ trunk/pyweek6/lib/actors.py
@@ -0,0 +1,33 @@
+from pyglet import event
+from widget import Widget
+
+class Actor(event.EventDispatcher):
+    def __init__(self):
+        pass
+        
+    def update(self,dt):
+        pass
+        
+    
+class Conveyor(Actor, Widget): # should inherit from actors as well so we can call update
+    def __init__(self):
+        #find out how big the conveyor is and where it sits then call:
+        Widget.__init__(self,0,200,700,100)
+
+    def do_click_action(self,x,y):
+        print "clicked the conveyor at (%i,%i) " %(x,y)
+
+class Robot(Actor, Widget):
+    pass
+    
+class Claw(Actor):
+    pass
+    
+class PartsBin(Actor, Widget):
+    pass
+    
+class FinishedRobotShelf(Widget): #this may need a better name
+    pass
+
+class Clock(Actor):
+    pass
--- trunk/pyweek6/lib/intro.py
+++ trunk/pyweek6/lib/intro.py
@@ -6,13 +6,13 @@ import data
 class Intro(event.EventDispatcher):
     # Timer duration in seconds.
     timerDuration = 5
-
+    background = image.load(data.filepath('intro.png'))
     def __init__(self):
         # Reset timer.
         self.timerCurrent = 0
 
     def update(self,dt):
-        self.image.blit(0,0)
+        Intro.background.blit(0,0)
         # Check for timer duration.
         if self.timerCurrent > self.timerDuration:
             self.finish()
@@ -24,8 +24,11 @@ class Intro(event.EventDispatcher):
         self.finish()
         return True
 
+    def on_mouse_press(self, x, y, button, modifiers):
+        self.finish()
+        return True
+
     def finish(self):
         self.dispatch_event('on_intro_finish')
 
-Intro.image = image.load(data.filepath('intro.png'))
 Intro.register_event_type('on_intro_finish')
