1f9e5368aebef03abc2935f497a01d9e5f7542ad
holomorph <holomorph@46f8a242-7348-0410-acc9-3907ed1566ce>
2008-04-02 04:02:16 +0000

    added game bacground. click coordinates position conveyor correctly

--- pyweek6/lib/actors.py
+++ pyweek6/lib/actors.py
@@ -6,7 +6,7 @@ class Conveyor(Actor, Widget): # should inherit from actors as well so we can ca
     def __init__(self, parent, imageName):
         #find out how big the conveyor is and where it sits then call:
         Widget.__init__(self,parent, 0,200,700,100)
-        Actor.__init__(self, imageName)
+        Actor.__init__(self, imageName, x=0, y=172)
 
     def do_click_action(self,x,y):
         print "clicked the conveyor at (%i,%i) " %(x,y)
